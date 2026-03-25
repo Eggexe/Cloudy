56e5e479014c552af56c2c7614f918fcd5322ac8
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-03-25 09:10:05 +0000

    add warn to misc.h

--- Render/misc.h
+++ Render/misc.h
@@ -6,4 +6,13 @@
 void CDY_Pause(int time);
 void CDY_Close();
 
+/* WARN
+ *
+ * CODE HERE WILL BE MOVED OUT OF MISC
+ * DUE TO SHIT DESIGN
+ * WHEN WILL IT BE MOVED?
+ * WHO KNOWS
+ *
+ */
+
 #endif

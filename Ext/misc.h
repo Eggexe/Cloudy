0a8479cd0c1ea2a7143544095993d42561d961a9
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-04-28 16:39:52 +0100

    Adds comments to misc.h for future dev

--- Ext/misc.h
+++ Ext/misc.h
@@ -4,11 +4,16 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_stdinc.h>
 
+// Pauses the current loop with time in ms
 void CDY_Pause(float time);
+
+// Closes the engine, good to have at the end of the main function
 void CDY_Close();
 
 
 /* FPS MANAGEMENT */
+
+// Holds information regarding FPS tracking
 typedef struct {
     int frame_counter;
     Uint64 fps_timer;
@@ -16,17 +21,18 @@ typedef struct {
     float target_frame_time;
 } CDY_FPSManager;
 
+// Does fancy maths with the integer passed to reach that target FPS
 CDY_FPSManager *CDY_FPSManagerCreate(int target_fps);
+
+
+// FPSBegin and FPSEnd is not required to be used
+// These are tracking functions, FPSManger is the only key function
+// Starts FPS tracking
 void CDY_FPSBegin(CDY_FPSManager *fps);
+// Ends FPS tracking
 void CDY_FPSEnd(CDY_FPSManager *fps);
 
-/* WARN
- *
- * CODE HERE WILL BE MOVED OUT OF MISC
- * DUE TO SHIT DESIGN
- * WHEN WILL IT BE MOVED?
- * WHO KNOWS
- *
- */
+// Code within this file will be moved to a more specialised
+// location at some point
 
 #endif

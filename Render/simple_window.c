6bea3faf50fd86103ed128d07d2573d860185c67
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-04-28 09:43:09 +0100

    Destroy window, init ttf

--- Render/simple_window.c
+++ Render/simple_window.c
@@ -1,10 +1,17 @@
 #include "simple_window.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 
 CDY_Simple_Window *CDY_SimpleWindowCreate(const char *title, int width, int height) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Init video error %s\n", SDL_GetError());
+        return NULL;
+    }
+    if (TTF_Init() != 0) {
+        printf("TTF init error %s\n", TTF_GetError());
         return NULL;
     }
 
@@ -25,4 +32,12 @@ CDY_Simple_Window *CDY_SimpleWindowCreate(const char *title, int width, int heig
     cdy_window->renderer = renderer;
 
     return cdy_window;
+}
+
+
+void CDY_SimpleWindowDestroy(CDY_Simple_Window *window) {
+    TTF_Quit();
+    SDL_DestroyRenderer(window->renderer);
+    SDL_DestroyWindow(window->sdl_window);
+    free(window);
 }

a6f588fd251cf9f84bfa15c3f132fe55932f3a9f
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-04-29 21:30:10 +0100

    Adds comments to simple_window.h for future dev

--- Render/simple_window.h
+++ Render/simple_window.h
@@ -5,16 +5,16 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 
+// SDL abstractions for a single-colour window
 typedef struct {
     SDL_Window *sdl_window;
     SDL_Renderer *renderer;
-} CDY_Simple_Window; // this makes it the word
+} CDY_Simple_Window;
 
+// Creates a simple, single colour window
 CDY_Simple_Window *CDY_SimpleWindowCreate(const char *title, int width, int height);
-void CDY_SimpleWindowDestroy(CDY_Simple_Window *window); // parameters here
+
+// Destroys said window
+void CDY_SimpleWindowDestroy(CDY_Simple_Window *window);
 
 #endif
-// simple window definition ft window + rend
-// mostly a proof of concept cuz this is rlly useless
-//
-// WARN ** THIS WILL BE REFACTORED AFTER V1

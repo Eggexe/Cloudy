967eef1e6f2f2a241de6f7ff8732583bdfca5101
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-05-12 10:18:33 +0100

    Remove everything and rebuild

--- Events/events.h
+++ Events/events.h
@@ -1,28 +1,8 @@
 #ifndef EVENTS_H
 #define EVENTS_H
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-
-// Keyboard events to raise for future events
-// QUIT: Close game
-// Keydown: Key is pressed down
-// Keyup: Key is released
-// None: No event
-typedef enum {
-    CDY_EVENT_QUIT,
-    CDY_EVENT_KEYDOWN,
-    CDY_EVENT_KEYUP,
-    CDY_EVENT_NONE
-} CDY_EventType;
-
-// Places event types into CDY_Event for function usage
 typedef struct {
-    CDY_EventType type;
-} CDY_Event;
-
-// Asks for an event
-// *event will require the address of event (&)
-int CDY_PollEvent(CDY_Event *event);
+    int id;
+} CDY_Entity;
 
 #endif

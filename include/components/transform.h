9774935d63124b32c5e4bbfd9a70a42f5af7d3b0
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-05-14 10:41:16 +0100

    Basic components for positions

---
+++ include/components/transform.h
@@ -0,0 +1,9 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+// Stores position and size of entity in a 2D environment
+typedef struct {
+    float x, y, w, h;
+} CDY_TransformComponent;
+
+#endif
---
+++ include/components/velocity.h
@@ -0,0 +1,8 @@
+#ifndef VELOCITY_H
+#define VELOCITY_H
+
+typedef struct {
+    float vx, vy;
+} CDY_VelocityComponent;
+
+#endif

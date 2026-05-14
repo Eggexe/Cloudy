b5e9df9f013bd3e180f84b789b428dbd9770c7f0
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-05-14 14:44:49 +0100

    Adds flat ECS world view for engine

---
+++ include/world.h
@@ -0,0 +1,44 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <stdint.h>
+#include <stdint.h>
+#include "entity/entity.h"
+#include "components/transform.h"
+#include "components/rectangle.h"
+#include "components/sprite.h"
+#include "components/velocity.h"
+
+#define CDY_MAX_ENTITIES 64
+
+// Masks for components
+// Components are stored into a 32 bit integer
+// 0000 <- component-less entity
+// 0001 <- entity now has componentX
+// 0011 <- entity now has componentX and Y
+// |= give component
+// &= remove component
+// &  check if has component
+
+#define COMPONENT_TRANSFORM (1 << 0) // 0001
+#define COMPONENT_VELOCITY  (1 << 1) // 0010
+#define COMPONENT_RECTANGLE (1 << 2) // 0100
+#define COMPONENT_SPRITE    (1 << 3) // 1000
+
+// Struct holds every engine state
+// Entites
+// Components
+// Component masks
+// Given to every system to allow them to read entity data
+typedef struct {
+    CDY_Entity entities[CDY_MAX_ENTITIES];
+    uint32_t component_mask[CDY_MAX_ENTITIES];
+    uint32_t entity_count;
+
+    CDY_TransformComponent  transforms[CDY_MAX_ENTITIES];
+    CDY_VelocityComponent   velocities[CDY_MAX_ENTITIES];
+    CDY_RectComponent       rectangles[CDY_MAX_ENTITIES];
+    CDY_SpriteComponent        sprites[CDY_MAX_ENTITIES];
+} CDY_World;
+
+#endif

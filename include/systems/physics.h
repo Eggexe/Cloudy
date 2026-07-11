7513b5a96bc70b7fdba9d319761473038c931025
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-07-11 16:14:04 +0100

    Cool system stuff

--- include/systems/physics.h
+++ include/systems/physics.h
@@ -3,6 +3,8 @@
 
 #include "../../src/world.c"
 
+// SysPhysicsUpdate requires both transform and velocity to read from and write to
+// It reads the velocity component and updates the entity transform by that much * dt
 void CDY_SysPhysicsUpdate(CDY_World *world, float dt);
 
 #endif

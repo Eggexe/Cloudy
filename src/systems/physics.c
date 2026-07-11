b26079d14a9c954b3ed5ec1dde7ea7478a6a1497
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-07-11 16:14:48 +0100

    Fix small error with reversed logic

--- src/systems/physics.c
+++ src/systems/physics.c
@@ -1,11 +1,13 @@
 #include "../../include/systems/physics.h"
 
+// SysPhysicsUpdate requires both transform and velocity to read from and write to
+// It reads the velocity component and updates the entity transform by that much * dt
+
 void CDY_SysPhysicsUpdate(CDY_World *world, float dt) {
     for (int i = 0; i < world->entity_count; i++)
     {
         // i used as internal ID here
-        if (CDY_WldCheckComponent(world, i, COMPONENT_VELOCITY) == 0
-            && CDY_WldCheckComponent(world, i, COMPONENT_TRANSFORM) == 0)
+        if (CDY_WldCheckComponent(world, i, COMPONENT_VELOCITY) && CDY_WldCheckComponent(world, i, COMPONENT_TRANSFORM) )
         {
             world->transforms[i].x += world->velocities[i].vx * dt;
             world->transforms[i].y += world->velocities[i].vy * dt;

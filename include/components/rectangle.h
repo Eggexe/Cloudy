14515abc1f9a58d19a221a1951500b9bbf26e32a
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-07-12 11:36:48 +0100

    Adds comments to method

--- include/components/rectangle.h
+++ include/components/rectangle.h
@@ -3,6 +3,11 @@
 
 // Solid colour rectangle data
 #include <stdint.h>
+
+// CDY_RectComponent holds only a local width and height for the rectangle
+// with an RGBA value too.
+// GLOBAL position for the rectangle should be found in a CDY_TransformComponent.
+// Ensure both are applied to an entity to allow it to move.
 typedef struct {
     int w, h;
     int8_t r, g, b, a; // Red Green Blue Alpha

a39828d280e05383dba8c9664a444611455433e4
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-07-12 11:37:00 +0100

    Adds comments to method

--- include/components/sprite.h
+++ include/components/sprite.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+// Ensure to add a definition for this later, I havent used it yet as im focusing on rectangles atm so ill add instructions in a bit
 typedef struct {
     uint32_t tex_id;
     int src_x, src_y,

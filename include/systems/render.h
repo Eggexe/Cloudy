a86da64709a08b4a694522fe929aab78ef557692
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-07-12 11:15:13 +0100

    Adds comments to method and removes warning at end

--- include/systems/render.h
+++ include/systems/render.h
@@ -5,6 +5,8 @@
 
 typedef struct CDY_Renderer CDY_Renderer;
 
+// SysRenderUpdate will be the primary method for rendering the current frame
+// How exactly rendering works will differ in the future, will be updated in the future with more accurate information
 void CDY_SysRenderUpdate(CDY_World *world, CDY_Renderer *renderer);
 
-#endif RENDER_H
+#endif

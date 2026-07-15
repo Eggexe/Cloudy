712f4f579131b86f76b7530fac847ab57be407a1
Eggexe <159236595+Eggexe@users.noreply.github.com>
2026-07-15 22:33:32 +0100

    Half work on sysrenderupdate, namechange pending

--- src/systems/render.c
+++ src/systems/render.c
@@ -1,7 +1,7 @@
 #include "../../include/systems/render.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
-#include <sys/types.h>
 
 // Reminder for future use SDL only found internally (in /src) hence the struct in render.h to abstract SDL stuff
 
@@ -10,5 +10,24 @@ typedef struct CDY_Renderer{
 } CDY_Renderer;
 
 void CDY_SysRenderUpdate(CDY_Registry *registry, CDY_Renderer *renderer) {
+    for (uint32_t i = 0; i < registry->entity_count; i++)
+    {
+        // BOTH CDY_RectComponent and CDY_TransformComponent are required to do this specific operation
+        // Ensure that the entity has both these components
+        // Fear not if they don't, they will be skipped over by this check and the game (should) proceed without errors
+        // yo why am i typing notes for developers ur not supposed to see this js go read the documentation whenever i get to writing it
 
+        if (CDY_RegCheckComponent(registry, i, COMPONENT_TRANSFORM) && CDY_RegCheckComponent(registry, i, COMPONENT_RECTANGLE))
+        {
+            SDL_Rect r;
+            // Transforms hold positions
+            r.x = registry->transforms[i].x;
+            r.y = registry->transforms[i].y;
+
+            // Rect holds the width and height
+            r.h = registry->rectangles[i].h;
+            r.w = registry->rectangles[i].w;
+        }
+        // TODO: give rect colours
+    }
 }
